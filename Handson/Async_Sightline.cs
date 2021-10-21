a86f5c11c9b19b603955d9d02e7012b780114701
keiwatanabe0218 <t10005kw@gmail.com>
2021-10-21 23:14:45 +0900

    initial commit

---
+++ Handson/Async_Sightline.cs
@@ -0,0 +1,121 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using GrasshopperAsyncComponent;
+using System.Windows.Forms;
+using Rhino.Geometry;
+
+namespace GrasshopperAsyncComponentDemo.SampleImplementations
+{
+    public class Async_SightlineComponent : GH_AsyncComponent
+    {
+        public override Guid ComponentGuid { get => new Guid("824441b2-6f70-459c-b5d5-b8a0260f2424"); }
+
+        protected override System.Drawing.Bitmap Icon { get => Properties.Resources.logo32; }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+      public Async_SightlineComponent() : base("Async Sightline", "Async Sightline", "check sight-lines", "Async Handson", "Samples")
+        {
+            BaseWorker = new SightlineWorker();
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddPointParameter("Points", "P", "Points", GH_ParamAccess.list);
+            pManager.AddPointParameter("Target", "T", "Target Point", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Radius", "R", "Radius", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddIntegerParameter("Result", "R", "Result", GH_ParamAccess.list);
+        }
+
+        public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
+        {
+            base.AppendAdditionalMenuItems(menu);
+            Menu_AppendItem(menu, "Cancel", (s, e) =>
+            {
+                RequestCancellation();
+            });
+        }
+    }
+
+    public class SightlineWorker : WorkerInstance
+    {
+        List<Point3d> iPoints = new List<Point3d>();
+        Point3d iTarget = new Point3d();
+        double iRadius = 0.0;
+        int iIterations = 0;
+        List<int> oResults = new List<int>();
+
+
+        public SightlineWorker() : base(null) { }
+
+        public override void DoWork(Action<string, double> ReportProgress, Action Done)
+        {
+            // Checking for cancellation
+            if (CancellationToken.IsCancellationRequested) { return; }
+
+            
+            List<Sphere> TestSpheres = new List<Sphere>();
+            for (int i = 0; i < iIterations; i++)
+            {
+                TestSpheres.Clear();
+                for (int j = 0; j < iIterations; j++)
+                {
+                    if (i != j) { TestSpheres.Add(new Sphere(iPoints[j], iRadius)); }
+                }
+                var res = CheckSightLine(iPoints[i], TestSpheres, iTarget);
+                oResults.Add(res);
+
+                ReportProgress(Id, ((double)(i + 1) / (double)iIterations));
+
+                // Checking for cancellation
+                if (CancellationToken.IsCancellationRequested) { return; }
+            }
+
+
+            Done();
+        }
+
+        public override WorkerInstance Duplicate() => new SightlineWorker();
+
+        public override void GetData(IGH_DataAccess DA, GH_ComponentParamServer Params)
+        {
+            if (CancellationToken.IsCancellationRequested) return;
+
+            
+            DA.GetDataList("Points", iPoints);
+            DA.GetData(1, ref iTarget);
+            DA.GetData(2, ref iRadius);
+            iIterations = iPoints.Count;
+
+        }
+
+        public override void SetData(IGH_DataAccess DA)
+        {
+            if (CancellationToken.IsCancellationRequested) return;
+            DA.SetDataList(0, oResults);
+        }
+
+        public int CheckSightLine(Point3d pt, List<Sphere> TestSpheres, Point3d target)
+        {
+            var count = 0;
+            var l = new Line(pt, target);
+            Point3d pt1;
+            Point3d pt2;
+            for (int i = 0; i < TestSpheres.Count; i++)
+            {
+                var events = Rhino.Geometry.Intersect.Intersection.LineSphere(l, TestSpheres[i], out pt1, out pt2);
+                if (events != Rhino.Geometry.Intersect.LineSphereIntersection.None) { count += 1; }
+            }
+            return count;
+        }
+    }
+
+}
---
+++ Handson/Sightline.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace GrasshopperAsyncComponentDemo.SampleImplementations
+{
+    public class Sightline : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public Sightline()
+          : base("Sightline", "Sightline",
+              "check sight-lines",
+              "Async Handson", "Samples")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddPointParameter("Points", "P", "Points", GH_ParamAccess.list);
+            pManager.AddPointParameter("Target", "T", "Target Point", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Radius", "R", "Radius", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddIntegerParameter("Result", "R", "Result", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            /// Initialize
+            List<Point3d> iPoints = new List<Point3d>();
+            Point3d iTarget = new Point3d();
+            double iRadius = 0.0;
+            int iIterations = 0;
+            List<int> oResults = new List<int>();
+
+            /// Get Data
+            DA.GetDataList("Points", iPoints);
+            DA.GetData(1, ref iTarget);
+            DA.GetData(2, ref iRadius);
+            iIterations = iPoints.Count;
+
+            /// Check Sight Line
+            List<Sphere> TestSpheres = new List<Sphere>();
+            for (int i = 0; i < iIterations; i++)
+            {
+                TestSpheres.Clear();
+                for (int j = 0; j < iIterations; j++)
+                {
+                    if (i != j) { TestSpheres.Add(new Sphere(iPoints[j], iRadius)); }
+                }
+                var res = CheckSightLine(iPoints[i], TestSpheres, iTarget);
+                oResults.Add(res);
+            }
+
+            /// Set Data
+            DA.SetDataList(0, oResults);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("58FA6AA3-D5E1-4E4A-9D4D-467ED58DD380"); }
+        }
+
+        public int CheckSightLine(Point3d pt, List<Sphere> TestSpheres, Point3d target)
+        {
+            var count = 0;
+            var l = new Line(pt, target);
+            Point3d pt1;
+            Point3d pt2;
+            for (int i = 0; i < TestSpheres.Count; i++)
+            {
+                var events = Rhino.Geometry.Intersect.Intersection.LineSphere(l, TestSpheres[i], out pt1, out pt2);
+                if (events != Rhino.Geometry.Intersect.LineSphereIntersection.None) { count += 1; }
+            }
+            return count;
+        }
+    }
+}
